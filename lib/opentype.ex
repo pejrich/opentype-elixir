2269245eb74906833965640133d91bb47ac477e9
John C Barstow <jbowtie@amathaine.com>
2017-07-31 22:02:07 +1200

    Move OpenTypeFont to OpenType.Font

--- lib/opentype.ex
+++ lib/opentype.ex
@@ -1,54 +1,8 @@
-defmodule OpenTypeFont do
-  use GenServer
-  alias OpenType
-  alias UnicodeData
-
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts)
-  end
-
-  def init(:ok) do
-    {:ok, OpenType.new()}
-  end
-
-  # read in and parse a TTF or OTF file
-  def parse(pid, filename) do
-    GenServer.cast(pid, {:parse, filename})
-    pid
-  end
-
-  # layout a run of text
-  # send back the glyphs and positioning data
-  def layout(pid, text, features \\ nil) do
-    GenServer.call(pid, {:layout, text, features})
-  end
-
-  # get the font structure (for additional analysis)
-  def font_structure(pid) do
-    GenServer.call(pid, :ttf)
-  end
-
-  def scale_factor(pid) do
-    GenServer.call(pid, :scale)
-  end
-
-  def handle_cast({:parse, filename}, ttf) do
-    parsed = OpenType.parse(ttf, filename)
-    {:noreply, parsed}
-  end
-  def handle_call(:ttf, _from, ttf) do
-    {:reply, ttf, ttf}
-  end
-  def handle_call(:scale, _from, ttf) do
-    {:reply, 1000.0 / ttf.unitsPerEm, ttf}
-  end
-  def handle_call({:layout, text, features}, _from, ttf) do
-    output = OpenType.layout_text(ttf, text, features)
-    {:reply, output, ttf}
-  end
-end
-
 defmodule OpenType do
+  @moduledoc """
+  Provides parsers and utilities
+  """
+
   alias OpenType.Parser
   alias OpenType.Substitutions
   alias OpenType.Positioning
